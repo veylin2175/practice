32eb1f6f2ba6042f93b0dd7d8cc5f6f3bbfaea26
вашÐveylin2175 <veylin2175@mail.ru>
2025-07-12 21:37:15 +0300

    L1.7 added

--- l1.7.go
+++ l1.7.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	var (
+		wg   sync.WaitGroup
+		mu   sync.Mutex
+		mapa = make(map[int]string)
+	)
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			mu.Lock()
+			mapa[n] = "value" + strconv.Itoa(n)
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println(mapa)
+}
