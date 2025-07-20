40a7caa67777c667295dd9a5ff922c007ba03317
вашÐveylin2175 <veylin2175@mail.ru>
2025-07-21 01:14:10 +0300

    Small bugs fixed

--- 23/l1.23.go
+++ 23/l1.23.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	list := []int{1, 2, 3, 4, 6, 7, 8, 10, 11, 13, 14, 16, 17, 18, 19}
@@ -12,21 +15,19 @@ func main() {
 		return
 	}
 
-	newList := delEl(list, index)
+	newList, err := delEl(list, index)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	fmt.Println(newList)
 }
 
-func delEl(list []int, el int) []int {
-	newList := make([]int, len(list)-1)
-	for k := range list {
-		if k < el {
-			newList[k] = list[k]
-		} else if k != len(list)-1 && k >= el {
-			newList[k] = list[k+1]
-		} else {
-			continue
-		}
+func delEl(list []int, el int) ([]int, error) {
+	if el < 0 || el > len(list) {
+		return nil, errors.New("index out of range")
 	}
 
-	return newList
+	return append(list[:el], list[el+1:]...), nil
 }
--- 25/l1.25.go
+++ 25/l1.25.go
@@ -11,6 +11,10 @@ func main() {
 	fmt.Println("Запуск остановки")
 	sleep(timer)
 	fmt.Println("Остановка завершена")
+
+	fmt.Println("Второй запуск")
+	sleep2(time.Second * 10)
+	fmt.Println("Второе время прошло")
 }
 
 func sleep(timer <-chan time.Time) {
@@ -18,4 +22,9 @@ func sleep(timer <-chan time.Time) {
 	case <-timer:
 		return
 	}
+}
+
+// Еще одна реализация, с передачей time. Duration вместо канала (как в оригинальном time. Sleep)
+func sleep2(timer time.Duration) {
+	<-time.After(timer)
 }
