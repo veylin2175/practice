49bd03be5edb139caf2d7dacdb2210f6941bb434
вашÐveylin2175 <veylin2175@mail.ru>
2025-07-20 20:39:21 +0300

    Added L1.21

---
+++ 21/l1.21.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type dog struct {
+	name string
+}
+
+func (d *dog) Bark() {
+	fmt.Println(d.name, "гавкает.")
+}
+
+type cat struct {
+	name string
+}
+
+func (c *cat) Meow() {
+	fmt.Println(c.name, "мяукает.")
+}
+
+type adapter interface {
+	reaction()
+}
+
+type dogAdapter struct {
+	*dog
+}
+
+func (d *dogAdapter) reaction() {
+	d.Bark()
+}
+
+func newDogAdapter(d *dog) adapter {
+	return &dogAdapter{d}
+}
+
+type catAdapter struct {
+	*cat
+}
+
+func (c *catAdapter) reaction() {
+	c.Meow()
+}
+
+func newCatAdapter(c *cat) adapter {
+	return &catAdapter{c}
+}
+
+func main() {
+	animals := [...]adapter{newDogAdapter(&dog{"Песик"}), newCatAdapter(&cat{"Китик"}), &cow{}}
+
+	for _, animal := range animals {
+		animal.reaction()
+	}
+}
+
+// Спокойно можем добавить еще животных:
+
+type cow struct{}
+
+func (c *cow) reaction() {
+	fmt.Println("Кто-то мычит...")
+}
